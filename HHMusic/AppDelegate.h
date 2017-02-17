dfd3b84416187c1a5b48e8750c8ff6ddce283bb4
邹伟芳 <weifang.zou@ttpai.cn>
2017-02-17 16:12:36 +0800

    音乐播放器首次提交

---
+++ HHMusic/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  HHMusic
+//
+//  Created by LXH on 2017/1/11.
+//  Copyright © 2017年 HHLM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+#import "HHAppdelegate.h"
+@interface AppDelegate : HHAppdelegate <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
